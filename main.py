73dfbea9dfe41816bec5fcc401860ff87a9acc8b
chyun7114 <77336660+chyun7114@users.noreply.github.com>
2024-09-26 17:41:58 +0900

    HW#1 add remote repository

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+x = int(input("input the first number : "))
+y = int(input("input the second number : "))
+
+print(f'add two numbers : {add(x, y)}')
+print(f'substract two numbers : {sub(x, y)}')
