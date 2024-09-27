002d1556901a91f9fdff481b89dd7bf8f6e3ea37
chyun7114 <77336660+chyun7114@users.noreply.github.com>
2024-09-27 16:11:14 +0900

    feature-addition branch new update

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "can't divided by 0"
+
+    return a / b
+
 x = int(input("input the first number : "))
 y = int(input("input the second number : "))
 
 print(f'add two numbers : {add(x, y)}')
 print(f'substract two numbers : {sub(x, y)}')
+print(f'multiply two numbers : {add(x, y)}')
+print(f'divide two numbers : {sub(x, y)}')
